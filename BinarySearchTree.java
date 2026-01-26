6ee3e1f459b5a0f37f592388f7c860ca276c8236
Aditya Harshavardhan <adiharsh9@gmail.com>
2026-01-26 10:55:34 -0600

    Finished Insert Method

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -5,8 +5,39 @@ public class BinarySearchTree<T extends Comparable<T>> implements SortedCollecti
     }
 
     //insert
+    public void insert(T data) throws NullPointerException {
+        if (data == null) {
+            throw new NullPointerException("Cant insert null into a BST");
+        }
+        BinaryNode<T> newNode = new BinaryNode<>(data);
+        if (root == null) {
+            root = newNode;
+        } else {
+            insertHelper(newNode, root);
+        }
+    }
 
     //insertHelper
+    protected void insertHelper(BinaryNode<T> newNode, BinaryNode<T> subtree) {
+        if (subtree == null) {
+            return;
+        }
+        if (newNode.getData().compareTo(subtree.getData()) <= 0) {
+            if (subtree.getLeft() == null) {
+                subtree.setLeft(newNode);
+                newNode.setUp(subtree);
+            } else {
+                insertHelper(newNode, subtree.getLeft());
+            }
+        } else {
+            if (subtree.getRight() == null) {
+                subtree.setRight(newNode);
+                newNode.setUp(subtree);
+            } else {
+                insertHelper(newNode, subtree.getRight());
+            }
+        }
+    }
 
     //contains
 
