5d3a5500c3acd25069d101bb2b103af6e173ef97
Aditya Harshavardhan <adiharsh9@gmail.com>
2026-01-25 19:10:28 -0600

    Made the Skeleton for P01

---
+++ BinarySearchTree.java
@@ -0,0 +1,18 @@
+public class BinarySearchTree<T extends Comparable<T>> implements SortedCollection<T> {
+    protected BinaryNode<T> root; // root node
+    public BinarySearchTree() {
+        root = null; // initially make it empty
+    }
+
+    //insert
+
+    //insertHelper
+
+    //contains
+
+    //size
+
+    //isEmpty
+
+    //clear
+}
