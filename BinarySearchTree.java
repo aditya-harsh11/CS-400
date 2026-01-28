9cccdbba846197a906ef0780a708c9cb6a54282b
Aditya Harshavardhan <adiharsh9@gmail.com>
2026-01-27 21:45:54 -0600

    Everything but test methods and main done

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,10 +1,15 @@
 public class BinarySearchTree<T extends Comparable<T>> implements SortedCollection<T> {
-    protected BinaryNode<T> root; // root node
+    protected BinaryNode<T> root;// root node
     public BinarySearchTree() {
-        root = null; // initially make it empty
+        root = null;
     }
 
-    //insert
+    /**
+     * Inserts a new data value into the sorted collection.
+     * @param data the new value being inserted
+     * @throws NullPointerException if data argument is null, we do not allow
+     */
+    @Override
     public void insert(T data) throws NullPointerException {
         if (data == null) {
             throw new NullPointerException("Cant insert null into a BST");
@@ -16,8 +21,12 @@ public class BinarySearchTree<T extends Comparable<T>> implements SortedCollecti
             insertHelper(newNode, root);
         }
     }
-
-    //insertHelper
+    
+    /**
+     * Helper method to insert values into the BST
+     * @param newNode the node to insert
+     * @param subtree the root of the subtree to insert into
+     */
     protected void insertHelper(BinaryNode<T> newNode, BinaryNode<T> subtree) {
         if (subtree == null) {
             return;
@@ -39,7 +48,13 @@ public class BinarySearchTree<T extends Comparable<T>> implements SortedCollecti
         }
     }
 
-    //contains
+    /**
+     * Check whether data is stored in the tree.
+     * @param find the value to check for in the collection
+     * @return true if the collection contains data one or more times, 
+     * and false otherwise
+     */
+    @Override
     public boolean contains(Comparable<T> find) {
         if (find == null) {
             return false;
@@ -57,9 +72,43 @@ public class BinarySearchTree<T extends Comparable<T>> implements SortedCollecti
         return false;
     }
 
-    //size
+    /**
+     * Counts the number of values in the collection, with each duplicate value
+     * being counted separately within the value returned.
+     * @return the number of values in the collection, including duplicates
+     */
+    @Override
+    public int size() {
+        return sizeHelper(root);
+    }
 
-    //isEmpty
+    /**
+     * Helper method to count the number of nodes in the BST
+     * @param node the root of the subtree to count nodes in
+     * @return the total number of nodes in the BST
+     */
 
-    //clear
+    private int sizeHelper(BinaryNode<T> node) {
+        if (node == null){
+            return 0;
+        }
+        return 1 + sizeHelper(node.getLeft()) + sizeHelper(node.getRight());
+    }
+
+    /**
+     * Checks if the collection is empty.
+     * @return true if the collection contains 0 values, false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    /**
+     * Removes all values and duplicates from the collection.
+     */
+    @Override
+    public void clear() {
+        root = null;
+    }
 }
